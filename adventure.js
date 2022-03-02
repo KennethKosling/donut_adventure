e99d50c41cf71e622ce674842d209eadb0e53d10
KennethKosling <kenkoz7164@gmail.com>
2022-03-01 23:00:38 -0500

    Created Pizza Rat

--- adventure.js
+++ adventure.js
@@ -1,4 +1,4 @@
-function getRandomInt(min, max) {
+function random(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min)
@@ -12,12 +12,12 @@ class Hero {
             sprinkleSpray: 5,
             sugarShock: 10
         };
-        this.catchPhrases = ['i\'m fresher than day old pizza',
-        'you can\'t count my calories']
+        this.catchPhrases = ['I\'m fresher than day old pizza',
+        'You can\'t count my calories']
     }
 
     talkSass(){
-        console.log(this.catchPhrases[getRandomInt(0, 2)])
+        console.log(this.catchPhrases[random(0, 2)])
     }
 
     announceHealth(){
@@ -25,10 +25,35 @@ class Hero {
     }
 
     fight(){
-        console.log('I\'m ready to rumble')
+        console.log('I\'m ready to rumble!')
     }
 }
 
 const dougie = new Hero('Dougie the Donut')
 
-dougie.talkSass()
+class Enemy {
+    constructor(name, health, weapons, catchPhrases) {
+        this.name = name;
+        this.health = 100;
+        this.weapons = {
+            pepperoniStars: 5,
+            cheeseGrease: 10
+        };
+        this.catchPhrases = ['I\'m youtube famous',
+        'I\'m more dangerous than an uncovered sewer']
+    }
+
+    talkSmack() {
+        console.log(this.catchPhrases[random(0,2)])
+    }
+
+    announceHealth() {
+        console.log(this.health)
+    }
+
+    fight() {
+        console.log('I\'m gonna flatten you like a slice of pepperoni!')
+    }
+}
+
+const rat = new Enemy('Pizza Rat')
